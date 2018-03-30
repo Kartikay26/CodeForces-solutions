ad8f4c218df0e17b80760017465cc07cb3f5aebb
Kumar Kartikay <krkartikay@gmail.com>
2018-03-30 12:04:00 +0530

    Add solution for problem `A - Diagonal Walking` Link: https://codeforces.com/contest/954/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/954/submission/36739069

--- codeforces/954/A.py
+++ codeforces/954/A.py
@@ -1,12 +1,28 @@
-import re
-
 def main():
     n = int(input())
     s = input()
-    n = 1 # dummy value
-    while n > 0:
-        s,n = re.subn("(RU|UR)","D",s)
-    print(len(s))
+    total_replaced = 0
+    d = 1 # dummy value
+    while d != 0:
+        d,s = replstr(s)
+        #print(d,s)
+        total_replaced += d
+    print(n - total_replaced)
+
+def replstr(s):
+    r = ""
+    d = 0
+    i = 0
+    while i < len(s):
+        z = s[i:i+2]
+        if z=="UR" or z=="RU":
+            r += "D"
+            d += 1
+            i += 1
+        else:
+            r += z[0]
+        i += 1
+    return d,r
 
 if __name__ == '__main__':
     main()
