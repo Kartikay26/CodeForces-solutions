a3471ffdfff869c363930b88f6f7611f33db3a46
Kumar Kartikay <krkartikay@gmail.com>
2018-08-27 22:36:00 +0530

    Add solution for problem `B - Unnatural Conditions` Link: https://codeforces.com/contest/1028/problem/B Tags: constructive algorithms, math, *1200 Ref: https://codeforces.com/contest/1028/submission/42167251

---
+++ codeforces/1028/B.py
@@ -0,0 +1,11 @@
+def main():
+	a,b = list(map(int, input().split()))
+	solve_data(a,b)
+
+def solve_data(n,m):
+	a = "54"*(2228//2)+"6"
+	b = "45"*(2228//2)+"4"
+	print(a,b)
+
+if __name__ == '__main__':
+	main()
