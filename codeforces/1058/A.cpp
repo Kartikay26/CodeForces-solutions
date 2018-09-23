b2e7ebd1b68af97dda6a3e00990cda6deec64309
Kumar Kartikay <krkartikay@gmail.com>
2018-09-23 18:37:00 +0530

    Add solution for problem `A - In Search of an Easy Problem` Link: https://codeforces.com/contest/1058/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1058/submission/43297514

---
+++ codeforces/1058/A.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+// #include "prettyprint.hpp"
+using namespace std;
+
+typedef long long int lli;
+#define int lli
+
+#define debug(x) cout << "debug: " << #x " = " << x << endl
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+
+int32_t main(){
+	fast_io();
+
+	int n; cin >> n;
+
+	bool easy = true;
+	for (int i = 0; i < n; ++i)
+	{
+		int x;
+		cin >> x;
+		if(x) easy = false;
+	}
+
+	if(easy){
+		cout << "EASY";
+	}
+	else{
+		cout << "HARD";
+	}
+
+	cout << endl;
+
+}
