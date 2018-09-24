281e2cbdca4285f12766a1a99150e2a1a8843036
Kumar Kartikay <krkartikay@gmail.com>
2018-09-25 02:10:00 +0530

    Add solution for problem `A - Way Too Long Words` Link: https://codeforces.com/contest/71/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/71/submission/43386677

--- codeforces/71/A.cpp
+++ codeforces/71/A.cpp
@@ -1,43 +1,35 @@
-// kartikay26
 #include <bits/stdc++.h>
+// #include "prettyprint.hpp"
 using namespace std;
+
+typedef long long int lli;
+#define int lli
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+#define debug(x) cout << "debug: " << #x " = " << x << endl
 #define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define all(x) x.begin(), x.end()
-#ifndef ONLINE_JUDGE
-#include "prettyprint.hpp"
-#define debug(...) cout << "debug: " << #__VA_ARGS__ " = "; _dbg(__VA_ARGS__);
-#define debuga(arr, n) _dbga(arr, n, #arr)
-#else
-#define debug(...)
-#define debuga(arr, n)
-#endif
-typedef long long int ll;
-#define int ll
-#define F first
-#define S second
-#define float double
-const int inf = LLONG_MAX;
-typedef double db;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
-void test()
-{
-	string s;
-	cin >> s;
-	if(s.length()>10) {
-		cout << s[0] << s.length()-2 << s[s.length()-1] <<endl;
-	} else {
-		cout << s << endl;
-	}
-}
-const int mod = 1e9+7;
-const int N = 1e6+1;
 
 int32_t main(){
 	fast_io();
-	int t;
-	cin >> t;
-	while(t--)
-		test();
-	return 0;
+
+	int n; cin >> n;
+
+	for (int i = 0; i < n; ++i)
+	{
+		string s;
+		cin >> s;
+		if (s.size()>10)
+		{
+			cout << s[0] << s.size() - 2 << s[s.size()-1] << endl;
+		}
+		else
+		{
+			cout << s << endl;
+		}
+	
+	}
+
 }
