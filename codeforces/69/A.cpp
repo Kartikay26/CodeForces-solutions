00e9ecc25c8137f73caaa48bbd1938ef8e930dca
Kumar Kartikay <krkartikay@gmail.com>
2018-09-25 02:01:00 +0530

    Add solution for problem `A - Young Physicist` Link: https://codeforces.com/contest/69/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/69/submission/43386437

--- codeforces/69/A.cpp
+++ codeforces/69/A.cpp
@@ -1,39 +1,43 @@
-// kartikay26
 #include <bits/stdc++.h>
+// #include "prettyprint.hpp"
 using namespace std;
-#ifdef LOCAL
-#include "prettyprint.hpp"
-#else
-#define debug(x...) 
-#endif
-typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
-#define int ll
-#define all(x) x.begin(), x.end()
-#define rall(x) x.rbegin(), x.rend()
-#define clr(x) memset(x, 0, sizeof(x))
+
+typedef long long int lli;
+#define int lli
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+#define debug(x) cout << "debug: " << #x " = " << x << endl
 #define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-void print(){cout << endl;} void prints(){} int get() {int x; cin >> x; return x;}
-template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
-template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
-template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
-template<typename T, typename... A> void prints(T x,A... r){cout << x;prints(r...);}
-
-const int mod = 1e9+7;
-const int N = 1e6+1;
-
-bool testcase(int){
-	int n = get();
-	int x = 0, y = 0, z = 0;
-	for (int i = 0; i < n; ++i) {
-		x += get();
-		y += get();
-		z += get();
-	}
-	return (x == 0 && y == 0 && z == 0);
-}
+#define all(x) x.begin(), x.end()
 
 int32_t main(){
 	fast_io();
-	print(testcase(0) ? "YES" : "NO");
-	return 0;
+
+	int n; cin >> n;
+
+	int x=0, y=0, z=0;
+
+	int a;
+
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> a;
+		x += a;
+		cin >> a;
+		y += a;
+		cin >> a;
+		z += a;
+	}
+
+	if (x == 0 and y == 0 and z == 0)
+	{
+		cout << "YES\n";
+	}
+	else
+	{
+		cout << "NO\n";
+	}
+
 }
