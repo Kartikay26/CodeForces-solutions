5812a36ba13a625e156ae39542dfacc9a5dc9be7
Kumar Kartikay <krkartikay@gmail.com>
2018-09-25 09:57:00 +0530

    Add solution for problem `A - Theatre Square` Link: https://codeforces.com/contest/1/problem/A Tags: math, *1000 Ref: https://codeforces.com/contest/1/submission/43394795

---
+++ codeforces/1/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+// #include "prettyprint.hpp"
+using namespace std;
+
+typedef long long int lli;
+#define int lli
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+#define debug(x) cout << "debug: " << #x " = " << x << endl
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+
+int32_t main(){
+	fast_io();
+
+	int n,m,a; cin >>n>>m>>a;
+	cout << int(ceil(double(n)/a)*ceil(double(m)/a)) << endl;
+
+}
