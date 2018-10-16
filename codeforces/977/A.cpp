1262f836f083f764522856287a733c07f640f3b4
Kumar Kartikay <krkartikay@gmail.com>
2018-10-16 12:50:00 +0530

    Add solution for problem `A - Wrong Subtraction` Link: https://codeforces.com/contest/977/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/977/submission/44388728

---
+++ codeforces/977/A.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+
+#ifndef ONLINE_JUDGE
+	#include "prettyprint.hpp"
+	#define debug(x) cout << "debug: " << #x " = " << x << endl
+#else
+	#define debug(x) 0
+#endif
+
+typedef long long int lli;
+#define int lli
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int32_t main(){
+	fast_io();
+	int n, k; cin >> n >> k;
+	for (int i = 0; i < k; ++i) {
+		if (n % 10 != 0) {
+			n--;
+		} else {
+			n /= 10;
+		}
+	}
+	cout << n << endl;
+}
