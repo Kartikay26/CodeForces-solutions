468aed98e599646823af2b10571bcaceb0a301e5
Kumar Kartikay <krkartikay@gmail.com>
2018-11-22 16:48:00 +0530

    Add solution for problem `C - k-Tree` Link: https://codeforces.com/contest/431/problem/C Tags: dp, implementation, trees, *1600 Ref: https://codeforces.com/contest/431/submission/46056652

--- codeforces/431/C.cpp
+++ codeforces/431/C.cpp
@@ -1,54 +1,47 @@
 // kartikay26
 #include <bits/stdc++.h>
 using namespace std;
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+#ifndef ONLINE_JUDGE
+#include "prettyprint.hpp"
+#define debug(...) cout << "debug: " << #__VA_ARGS__ " = " << make_tuple(__VA_ARGS__) << endl
+#define debuga(arr, n) dbga(arr, n, #arr)
+#else
+#define debug(...)
+#define debuga(arr, n)
+#endif
 typedef long long int ll;
 #define int ll
-#ifdef LOCAL
-#include "prettyprint.hpp"
-#else
-#define debug(x...) 
-#define print(x...) 
-#endif
+template<typename T> void dbga(T* a, int n, string s){
+	cout << "debug: " << s << "= [";
+	while(n--){cout << *a++ << (n?", ":"");} cout << "]\n";
+}
+#define F first
+#define S second
+typedef vector<int> vi;
+typedef pair<int, int> pii;
 
-int N, K, D;
 const int mod = 1e9+7;
+const int N = 1e6+1;
+int paths_k[N];
+int paths_d[N];
 
-int dp[101][101];
-
-int solve (int n, int d)
-{
-    if (n < d) {
-        return 0;
-    }
-    if (n <= 0) {
-        return n == 0 and d == 0;
-    }
-    if (dp[n][d]) {
-        return dp[n][d];
-    }
-    debug(n,d);
-    int ans = 0;
-    for (int i = 1; i <= K; ++i) {
-        if (i >= d)
-            ans += solve(n-i, 0);
-        else
-            ans += solve(n-i, d);
-        ans %= mod;
-    }
-    return dp[n][d] = ans;
-}
-
-void test()
-{
-    cin >> N >> K >> D;
-    debug(N, K, D);
-    cout << solve(N, D) << endl;
-}
-
-int32_t main()
-{
-    ios_base::sync_with_stdio(0); cin.tie(0);
-    test();
-    print("Time: ", (int)(clock() * 1000. / CLOCKS_PER_SEC), "ms");
-    return 0;
+int32_t main(){
+	fast_io();
+	int n, k, d;
+	cin >> n >> k >> d;
+	paths_k[0] = 1;
+	for (int i = 1; i <= n; ++i) {
+		for(int z = 1; z <= k; z++)
+			if(i>=z) {paths_k[i] += paths_k[i-z]; paths_k[i] %= mod;}
+	}
+	paths_d[0] = 1;
+	for (int i = 1; i <= n; ++i) {
+		for(int z = 1; z <= d-1; z++)
+			if(i>=z) {paths_d[i] += paths_d[i-z]; paths_d[i] %= mod;}
+	}
+	debuga(paths_k, n+1);
+	debuga(paths_d, n+1);
+	cout << (paths_k[n] - paths_d[n] + mod)%mod << endl;
 }
