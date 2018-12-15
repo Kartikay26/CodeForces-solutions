5f613b24b472c86f05a22a99317a8159a7fb49d2
Kumar Kartikay <krkartikay@gmail.com>
2018-12-15 20:11:00 +0530

    Add solution for problem `B - Letters Rearranging` Link: https://codeforces.com/contest/1093/problem/B Tags: constructive algorithms, greedy, sortings, strings, *900 Ref: https://codeforces.com/contest/1093/submission/47042973

---
+++ codeforces/1093/B.cpp
@@ -0,0 +1,50 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+#ifndef ONLINE_JUDGE
+#include "prettyprint.hpp"
+#define debug(...) cout << "debug: " << #__VA_ARGS__ " = " << make_tuple(__VA_ARGS__) << endl
+#define debuga(arr, n) dbga(arr, n, #arr)
+#else
+#define debug(...)
+#define debuga(arr, n)
+#endif
+typedef long long int ll;
+#define int ll
+template<typename T> void dbga(T* a, int n, string s){
+	cout << "debug: " << s << " = [";
+	while(n--){cout << *a++ << (n?", ":"");} cout << "]\n";
+}
+#define F first
+#define S second
+#define float double
+const int inf = LLONG_MAX;
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+void testcase(int t){
+	string x;
+	cin >> x;
+	sort(all(x));
+	string y = x;
+	reverse(all(y));
+	if(y == x)
+		cout << -1 << endl;
+	else
+		cout << x << endl;
+}
+
+int32_t main(){
+	fast_io();
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		testcase(i);
+	}
+}
