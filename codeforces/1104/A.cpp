9429540f8b30ce373ac11447b29747e918e32ecd
Kumar Kartikay <krkartikay@gmail.com>
2019-04-28 18:42:00 +0530

    Add solution for problem `A - Splitting into digits` Link: https://codeforces.com/contest/1104/problem/A Tags: constructive algorithms, implementation, math, *800 Ref: https://codeforces.com/contest/1104/submission/53471791

---
+++ codeforces/1104/A.cpp
@@ -0,0 +1,35 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+#ifndef ONLINE_JUDGE
+#include "prettyprint.hpp"
+#define debug(...) cout << "debug: " << #__VA_ARGS__ " = "; _dbg(__VA_ARGS__);
+#define debuga(arr, n) _dbga(arr, n, #arr)
+#else
+#define debug(...)
+#define debuga(arr, n)
+#endif
+typedef long long int ll;
+#define int ll
+#define F first
+#define S second
+#define float double
+const int inf = LLONG_MAX;
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+int32_t main(){
+	fast_io();
+	int n; cin >> n;
+	cout << n << endl;
+	for (int i = 0; i < n; ++i) {
+		cout << 1 << " ";
+	}
+	return 0;
+}
