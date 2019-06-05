02d97afab1eba3a6ec39f7f717d108cd661c134d
Kumar Kartikay <krkartikay@gmail.com>
2019-06-05 14:57:00 +0530

    Add solution for problem `A - Definite Game` Link: https://codeforces.com/contest/1081/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1081/submission/55117710

---
+++ codeforces/1081/A.cpp
@@ -0,0 +1,35 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#else
+#define debug(...)
+#define debuga(arr, n)
+#define debugg(grid, n) 
+#endif
+typedef long long int ll;
+#define int ll
+#define F first
+#define S second
+#define float double
+const int inf = LLONG_MAX;
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+int32_t main(){
+	fast_io();
+	int n; cin >> n;
+	if(n != 2){
+		cout << 1;
+	} else {
+		cout << 2;
+	}
+	return 0;
+}
