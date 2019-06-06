89d8bbe25b36173365a2bf8b6270fd04d4ed3c64
Kumar Kartikay <krkartikay@gmail.com>
2019-06-06 10:07:00 +0530

    Add solution for problem `A - Lucky Year` Link: https://codeforces.com/contest/808/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/808/submission/55187732

---
+++ codeforces/808/A.cpp
@@ -0,0 +1,33 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#else
+#define debug(...)
+#define debuga(arr, n)
+#define debugg(grid, n) 
+#endif
+typedef long long int ll;
+#define int ll
+#define F first
+#define S second
+#define float double
+const int inf = LLONG_MAX;
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+int32_t main(){
+	fast_io();
+	int n; cin >> n;
+	int k = pow(10, int(log10(n)));
+	int ans = k * ((n/k)+1) - n;
+	cout << ans;
+	return 0;
+}
