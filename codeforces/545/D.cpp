17c87f56dd1f66fb3cfc84ef38acddc3f59e70cf
Kumar Kartikay <krkartikay@gmail.com>
2019-08-16 15:50:00 +0530

    Add solution for problem `D - Queue` Link: https://codeforces.com/contest/545/problem/D Tags: greedy, implementation, sortings, *1300 Ref: https://codeforces.com/contest/545/submission/58904158

---
+++ codeforces/545/D.cpp
@@ -0,0 +1,47 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define all(x) x.begin(), x.end()
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#else
+#define debug(...)
+#define debuga(arr, n)
+#define debugg(grid, n) 
+#endif
+typedef long long int ll;
+#define int ll
+#define F first
+#define S second
+#define float double
+const int inf = LLONG_MAX;
+int get() { int x; cin >> x; return x; }
+void getl(int* ar, int sz){ for(int i=0; i<sz; i++) cin >> ar[i];}
+template <typename T> void print(T x) { cout << x << "\n";}
+template <typename T> void prints(T x) { cout << x << " ";}
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const int mod = 1e9+7;
+const int N = 1e5+1;
+
+int n, t[N];
+
+int32_t main(){
+	fast_io();
+	n = get();
+	getl(t, n);
+	sort(t, t+n);
+	int s = 0, ans = 0;
+	for (int i = 0; i < n; ++i) {
+		if (s <= t[i]){
+			ans++;
+			s += t[i];
+			debug(t[i]);
+		}
+	}
+	print(ans);
+	return 0;
+}
