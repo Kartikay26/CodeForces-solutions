cd2326a8280cd485933282ba5d094df38c733e39
Kumar Kartikay <krkartikay@gmail.com>
2019-09-29 19:32:00 +0530

    Add solution for problem `C - Primes and Multiplication` Link: https://codeforces.com/contest/1228/problem/C Tags: math, number theory, *1700 Ref: https://codeforces.com/contest/1228/submission/61493049

---
+++ codeforces/1228/C.cpp
@@ -0,0 +1,137 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+void print(){cout << endl;} void prints(){}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+typedef long long int ll;
+#define int ll
+const int inf = LLONG_MAX;
+typedef double db;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+template <uint32_t mod>
+class Modular {
+private:
+  uint32_t n;
+
+public:
+  Modular(int64_t _n = 0) : n((_n >= 0 ? _n : mod - (-_n) % mod) % mod) {}
+
+  uint32_t get() const {
+    return n;
+  }
+
+  bool operator ==(const Modular& o) const {
+    return n == o.n;
+  }
+
+  bool operator !=(const Modular& o) const {
+    return n != o.n;
+  }
+
+  Modular& operator +=(const Modular& o) {
+    n += o.n;
+    n = (n < mod ? n : n - mod);
+    return *this; 
+  }
+
+  Modular& operator -=(const Modular& o) {
+    n += mod - o.n;
+    n = (n < mod ? n : n - mod);
+    return *this;
+  }
+
+  Modular& operator *=(const Modular& o) {
+    n = uint64_t(n) * o.n % mod;
+    return *this;
+  }
+
+  Modular& operator /=(const Modular& o) {
+    return (*this) *= o.inv();
+  }
+
+  Modular operator +(const Modular& o) const {
+    return Modular(*this) += o;
+  }
+
+  Modular operator -(const Modular& o) const {
+    return Modular(*this) -= o;
+  }
+
+  Modular operator *(const Modular& o) const {
+    return Modular(*this) *= o;
+  }
+
+  Modular operator /(const Modular& o) const {
+    return Modular(*this) /= o;
+  }
+
+  Modular pow(uint64_t b) const {
+    Modular ans(1), m = Modular(*this);
+    while (b) {
+      if (b & 1) {
+        ans *= m;
+      }
+      m *= m;
+      b >>= 1;
+    }
+    return ans;
+  }
+
+  Modular inv() const {
+    int32_t a = n, b = mod, u = 0, v = 1;
+    while (a) {
+      int32_t t = b / a;
+      b -= t * a;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    assert(b == 1);
+    return Modular(u);
+  }
+};
+
+using mint = Modular<mod>;
+
+vi factorise(int num){
+	vi ans;
+	for(int i = 2; i * i <= num; i++){
+		if(num % i == 0){
+			num /= i;
+			ans.push_back(i);
+			i--;
+		}
+	}
+	if(num > 1) ans.push_back(num);
+	return ans;
+}
+
+int32_t main(){
+	fast_io();
+	int x, n; cin >> x >> n;
+	vi factors = factorise(x);
+	mint ans = 1;
+	set<int> f(all(factors));
+	for(auto p : f){
+		int z = n;
+		while (z > 0){
+			z /= p;
+			ans *= mint(p).pow(z);
+		}
+	}
+	print(ans.get());
+	return 0;
+}
