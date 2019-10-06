2760df179fae4f2dd060504639cf65ae4e4d40b8
Kumar Kartikay <krkartikay@gmail.com>
2019-10-06 20:54:00 +0530

    Add solution for problem `B - Strings Equalization` Link: https://codeforces.com/contest/1241/problem/B Tags: brute force, strings, *1000 Ref: https://codeforces.com/contest/1241/submission/62002300

---
+++ codeforces/1241/B.cpp
@@ -0,0 +1,52 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+void print(){cout << endl;} void prints(){}
+template<typename T=int> T get(){T x; cin >> x; return x;}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clear(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+bool testcase(int t){
+	
+	string s1, s2;
+	cin >> s1 >> s2;
+
+	bool ok = false;
+
+	bool a[26]; clear(a);
+	for(auto c : s1){
+		a[c-'a'] = 1;
+	}
+	for(auto c : s2){
+		ok |= a[c-'a'];
+	}
+
+	return ok;
+}
+
+int32_t main(){
+	fast_io();
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; ++i) {
+		print(
+			testcase(i) ? "yes" : "no"
+		);
+	}
+	return 0;
+}
