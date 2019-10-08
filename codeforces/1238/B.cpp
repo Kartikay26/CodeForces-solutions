ae44453688002df5ad8218f61a4e58cbed0d90a7
Kumar Kartikay <krkartikay@gmail.com>
2019-10-08 20:33:00 +0530

    Add solution for problem `B - Kill `Em All` Link: https://codeforces.com/contest/1238/problem/B Tags: greedy, sortings, *1300 Ref: https://codeforces.com/contest/1238/submission/62130876

---
+++ codeforces/1238/B.cpp
@@ -0,0 +1,59 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+void print(){cout << endl;} void prints(){}
+template<typename T=int> T get(){T x; cin >> x; return x;}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clear(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const int mod = 1e9+7;
+const int N = 1e5+1;
+
+void testcase(int t){
+	
+	int n = get();
+	int r = get();
+
+	set<int> x;
+	for (int i = 0; i < n; ++i) {
+		x.insert(get());
+	}
+
+	debug(x);
+
+	int ans = 0;
+	int killed_last = 0;
+	for (auto it = x.rbegin(); it != x.rend(); it++){
+		if (*it <= killed_last){
+			break;
+		}
+		killed_last += r;
+		ans++;
+		debug(ans, killed_last, *it);
+	}
+
+	print(ans);
+	
+}
+
+int32_t main(){
+	fast_io();
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; ++i) {
+		testcase(i);
+	}
+	return 0;
+}
