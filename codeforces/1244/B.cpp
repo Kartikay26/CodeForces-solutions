3c21fad828d4e632c5fefc5dd78745c40b9cc7c4
Kumar Kartikay <krkartikay@gmail.com>
2019-10-13 14:51:00 +0530

    Add solution for problem `B - Rooms and Staircases` Link: https://codeforces.com/contest/1244/problem/B Tags: brute force, implementation, *1000 Ref: https://codeforces.com/contest/1244/submission/62470819

---
+++ codeforces/1244/B.cpp
@@ -0,0 +1,48 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+void print(){cout << endl;} void prints(){}
+template<typename T=int> T get(){T x; cin >> x; return x;}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clear(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+void testcase(int t){
+	
+	int n; cin >> n;
+	string s; cin >> s;
+	if(s.find('1') == -1){
+		print(n); return;
+	}
+	int ans = 0;
+	for(int i = 0; i < n; i++){
+		if(s[i] == '1'){
+			ans = max(ans, max(i+1, n-i));
+		}
+	}
+	print(ans*2);
+}
+
+int32_t main(){
+	fast_io();
+	int t;
+	cin >> t;
+	for (int i = 1; i <= t; ++i) {
+		testcase(i);
+	}
+	return 0;
+}
