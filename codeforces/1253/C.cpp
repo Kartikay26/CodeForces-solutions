e4592d8b1b6c97029720830324a7e577d3c41245
Kumar Kartikay <krkartikay@gmail.com>
2019-11-16 21:02:00 +0530

    Add solution for problem `C - Sweets Eating` Link: https://codeforces.com/contest/1253/problem/C Tags: dp, greedy, math, sortings, *1500 Ref: https://codeforces.com/contest/1253/submission/65191680

---
+++ codeforces/1253/C.cpp
@@ -0,0 +1,51 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+void print(){cout << endl;} void prints(){}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+int32_t main(){
+    fast_io();
+
+    int n, m; cin >> n >> m;
+    vi a(n); get(a, n);
+    sort(all(a));
+
+    debug(a);
+
+    vi b(n);
+    
+    for (int i = 0; i < n; ++i) {
+        b[i] = a[i];
+        if(i - m >= 0){
+            b[i] += b[i-m];
+        }
+    }
+
+    debug(b);
+
+    int s = 0;
+    for(auto &i : b){
+        s += i;
+        cout << s << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
