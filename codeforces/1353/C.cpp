cab7c0db493ec8b86f3af63ee0cd836b68cafd8a
Kumar Kartikay <krkartikay@gmail.com>
2020-07-15 21:44:00 +0530

    Add solution for problem `C - Board Moves` Link: https://codeforces.com/contest/1353/problem/C Tags: math, *1000 Ref: https://codeforces.com/contest/1353/submission/86929572

---
+++ codeforces/1353/C.cpp
@@ -0,0 +1,48 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+void print(){cout << endl;} void prints(){}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+void testcase(int t){
+    
+    int n; cin >> n;
+    n = (n+1)/2;
+    int side_len = 1;
+    int num_sq = 0;
+    int ans = 0;
+    for (int i = 0; i < n; ++i, side_len += 2) {
+        num_sq = side_len*4 - 4;
+        ans += i * num_sq;
+        // debug(i, side_len, num_sq, ans);
+    }
+
+    print(ans);
+    
+}
+
+int32_t main(){
+    fast_io();
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        testcase(i);
+    }
+    return 0;
+}
