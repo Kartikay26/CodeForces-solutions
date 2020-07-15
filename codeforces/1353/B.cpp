7fe86b28657ad3a58bc1d739edaa92b0c8694d26
Kumar Kartikay <krkartikay@gmail.com>
2020-07-15 21:33:00 +0530

    Add solution for problem `B - Two Arrays And Swaps` Link: https://codeforces.com/contest/1353/problem/B Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1353/submission/86928865

---
+++ codeforces/1353/B.cpp
@@ -0,0 +1,54 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+void print(){cout << endl;} void prints(){}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+void testcase(int t){
+    
+    int n, k; cin >> n >> k;
+
+    vi a(n), b(n); get(a,n); get(b,n);
+
+    sort(all(a)); sort(rall(b));
+
+    debug(a);
+    debug(b);
+
+    int ans = 0;
+    for (int i = 0; i < min(k,n); ++i) {
+        ans += max(a[i],b[i]);
+    }
+    for (int i = min(k,n); i < n; ++i) {
+        ans += a[i];
+    }
+
+    print(ans);
+    
+}
+
+int32_t main(){
+    fast_io();
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        testcase(i);
+    }
+    return 0;
+}
