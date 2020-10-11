18b9d1b099f6d6e2f86d3a0c5c6cb93c5854e375
Kumar Kartikay <krkartikay@gmail.com>
2020-10-11 14:39:00 +0530

    Add solution for problem `A - Number of Apartments` Link: https://codeforces.com/contest/1430/problem/A Tags: brute force, constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1430/submission/95194539

---
+++ codeforces/1430/A.cpp
@@ -0,0 +1,46 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+void print(){cout << endl;} void prints(){} int get() {int x; cin >> x; return x;}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+void testcase(int t){
+    int n = get();
+    for (int a = 0; a < n; ++a) {
+        for (int b = 0; b < n; ++b) {
+            int c = n - 3*a - 5*b;
+            if (c >= 0 and c % 7 == 0) {
+                print(a,b,c/7);
+                return;
+            }
+        }
+    }
+    print(-1);
+    return;
+}
+
+int32_t main(){
+    fast_io();
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        testcase(i);
+    }
+    return 0;
+}
