d14fc0aaef555d41d10bf89a4547c1d724e47224
Kumar Kartikay <krkartikay@gmail.com>
2020-10-15 19:38:00 +0530

    Add solution for problem `A - Rational Resistance` Link: https://codeforces.com/contest/343/problem/A Tags: math, number theory, *1600 Ref: https://codeforces.com/contest/343/submission/95593397

---
+++ codeforces/343/A.cpp
@@ -0,0 +1,45 @@
+// kartikay26
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x...) 
+#ifdef LOCAL
+#include "prettyprint.hpp"
+#endif
+typedef long long int ll; typedef double db; typedef vector<ll> vi; typedef pair<ll, ll> pii;
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define fast_io() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+void print(){cout << endl;} void prints(){} int get() {int x; cin >> x; return x;}
+template<typename T> void get(T* x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T> void get(T& x, int n){for (int i = 0; i < n; ++i) { cin >> x[i]; }}
+template<typename T, typename... A> void print(T x,A... r){cout << x << " ";print(r...);}
+template<typename T, typename... A> void prints(T x,A... r){cout << x << " ";prints(r...);}
+
+const int mod = 1e9+7;
+const int N = 1e6+1;
+
+int solve(int a, int b) {
+	if (a > b) {
+		int e = a/b;
+		return e + solve(a-e*b, b);
+	} else if (a == 0) {
+		return 0;
+	} else {
+		return 1 + solve(b-a, a);
+	}
+	return 1;
+}
+
+void solve(){
+	int a = get();
+	int b = get();
+	print(solve(a,b));
+}
+
+int32_t main(){
+	fast_io();
+	solve();
+	return 0;
+}
